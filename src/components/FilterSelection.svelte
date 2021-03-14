2d8c094506f1ff3e6515cebd8217e415eed06c40
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-03-14 01:04:04 +0100

    fix(app): ?? do not build

--- src/components/FilterSelection.svelte
+++ src/components/FilterSelection.svelte
@@ -1,6 +1,6 @@
 <select bind:value={value} class='rounded-md dark:bg-gray-600 dark:text-white truncate'>
   <option value={-1}>All</option>
-  {#each options ?? [] as option}
+  {#each optionsGuard ?? [] as option}
     <option value={option.id}>{option.name} ({option.count})</option>
   {/each}
 </select>
@@ -9,7 +9,8 @@ import { createEventDispatcher } from 'svelte';
 import type { CategoryTag } from '../utils/amiamiUtils';
 
 export let value: number = -1;
-export let options: CategoryTag[] = [];
+export let options: CategoryTag[] = []
+$: optionsGuard = options !== null || options !== null ? options : []
 const dispatch = createEventDispatcher();
 
 </script>
